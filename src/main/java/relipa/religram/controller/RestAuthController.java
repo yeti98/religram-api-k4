674515f1db8345ee315e496b2a408e0c3efed189
Trần Đăng Khoa <khoatd@relipasoft.com>
2019-08-30 15:19:09 +0700

    Task #17: Verify token

--- src/main/java/relipa/religram/controller/RestAuthController.java
+++ src/main/java/relipa/religram/controller/RestAuthController.java
@@ -3,6 +3,8 @@ package relipa.religram.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,5 +51,13 @@ public class RestAuthController {
     @PostMapping("/resetpassword")
     public void resetPassword(@Valid @RequestBody ResetPasswordRequest resetPasswordRequest) {
         userService.resetPassword(resetPasswordRequest.getEmail());
+    }
+
+    @GetMapping("/verify/{token}")
+    public String verify(@PathVariable String token) {
+        if (jwtService.validateToken(token))
+            return "OK";
+        else
+            throw new RuntimeException("Token is invalid!");
     }
 }
