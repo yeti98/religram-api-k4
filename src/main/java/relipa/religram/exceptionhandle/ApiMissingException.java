407523735789037a10d5e4ad08fbebd2375a17ce
trangnh-relipasoft <54127069+trangnh-relipasoft@users.noreply.github.com>
2019-09-02 23:51:01 +0700

    Revert "K4 10"

--- src/main/java/relipa/religram/exceptionhandle/ApiMissingException.java
+++
@@ -1,8 +0,0 @@
-package relipa.religram.exceptionhandle;
-
-public class ApiMissingException extends RuntimeException {
-
-    public ApiMissingException() {
-        super("API key is missing or invalid");
-    }
-}
